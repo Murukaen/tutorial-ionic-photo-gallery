80d5142fb9174c31c26a46a514ed6b2ad99e467c
razvan <razzvan.savu@gmail.com>
2023-11-13 21:42:40 +0200

    Prepare tab 2 to store images

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,16 +1,19 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonFabButton, IonFab, IonIcon } from '@ionic/angular/standalone';
+import { camera } from 'ionicons/icons';
+import { addIcons } from 'ionicons';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent]
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonFabButton, IonFab, IonIcon]
 })
 export class Tab2Page {
 
-  constructor() {}
+  constructor() {
+    addIcons({ camera });
+  }
 
 }
